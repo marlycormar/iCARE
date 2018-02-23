b7abdbccbc32c378b6ca7d0eef5a54ef36296fa6
Marly Cormar <marlycormar@gmail.com>
2018-02-23 13:39:44 -0500

    Iterate through each file on the data folder

--- create_and_populate_table.py
+++ create_and_populate_table.py
@@ -1,29 +1,36 @@
 import csv
 import MySQLdb
+import os
+
+# writing as many %s as the number of columns
+number_columns = 55
+#number_columns += 2 # accounting for columns file_name and study_id
+format_strings = ','.join(['%s'] * number_columns)
 
 # connecting to the database
 mydb = MySQLdb.connect(host='localhost', user='root', db='iCARE')
 cursor = mydb.cursor()
 
-# creating the new columns
-file_name = 'UFH_00077_001_S5_R1_001_FarsightAllVars_VI0209_587genelist.csv';
-study_id = file_name.split('_')
-study_id = study_id[0] + '_' + study_id[1]
+# setting the directory to the read the files from
+directory = '/Users/marlycormar/Google Drive/CTS-IT/Tasks/20180220/data'
 
-# reading the data
-csv_data = csv.reader(file('../data/%s' %file_name))
-csv_data.next() # skip the first line
+for file_name in os.listdir(directory):
+    if file_name.endswith(".csv"):
+        print(file_name)
+        study_id = file_name.split('_')
+        study_id = study_id[0] + '_' + study_id[1]
 
-# writing as many %s as the number of columns
-number_columns = 55
-number_columns += 2 # accounting for columns file_name and study_id
-format_strings = ','.join(['%s'] * number_columns)
+        # reading the data
+        csv_data = csv.reader(file('../data/%s' %file_name), dialect=csv.excel_tab)
+        csv_data.next() # skip the first line as it contains the column names
 
-# insert each row
-for row in csv_data:
-    row = [study_id] + row      # consider improving this method of prepending
-    row = [file_name] + row     # maybe modify the csv file first?
-    cursor.execute("INSERT INTO test VALUES(%s)" %format_strings, row)
+        # insert each row
+        for row in csv_data:
+            row = [study_id] + row      # consider improving this method of prepending
+            row = [file_name] + row     # maybe modify the csv file first?
+            #cursor.execute("INSERT INTO test VALUES(%s)" %format_strings, row)
+    else:
+        continue
 
 # close the connection to the database.
 mydb.commit()
