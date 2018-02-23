632207970561a9e80a6070c3026118a2a44b23fd
Marly Cormar <marlycormar@gmail.com>
2018-02-23 15:57:53 -0500

    Check column names and then inserts the rows accordingly

--- create_and_populate_table.py
+++ create_and_populate_table.py
@@ -2,10 +2,7 @@ import csv
 import MySQLdb
 import os
 
-# writing as many %s as the number of columns
-number_columns = 55
-#number_columns += 2 # accounting for columns file_name and study_id
-format_strings = ','.join(['%s'] * number_columns)
+
 
 # connecting to the database
 mydb = MySQLdb.connect(host='localhost', user='root', db='iCARE')
@@ -15,20 +12,29 @@ cursor = mydb.cursor()
 directory = '/Users/marlycormar/Google Drive/CTS-IT/Tasks/20180220/data'
 
 for file_name in os.listdir(directory):
-    if file_name.endswith(".csv"):
+    if file_name.endswith(".csv") and file_name.startswith("UFH"):
         print(file_name)
         study_id = file_name.split('_')
         study_id = study_id[0] + '_' + study_id[1]
 
         # reading the data
-        csv_data = csv.reader(file('../data/%s' %file_name), dialect=csv.excel_tab)
-        csv_data.next() # skip the first line as it contains the column names
+        csv_data = csv.reader(file('../data/%s' %file_name))
+        #csv_data.next() # skip the first line as it contains the column names
+
+        headers = csv_data.next()
+        format_strings = ','.join(['%s'] * len(headers))
+        columns = ['`' + field_name  + '`' for field_name in headers]
+        columns = ",".join(columns)
+        print(columns)
+
 
         # insert each row
         for row in csv_data:
-            row = [study_id] + row      # consider improving this method of prepending
-            row = [file_name] + row     # maybe modify the csv file first?
-            #cursor.execute("INSERT INTO test VALUES(%s)" %format_strings, row)
+            # writing as many %s as the number of columns
+            #number_columns += 2 # accounting for columns file_name and study_id
+            #row = [study_id] + row      # consider improving this method of prepending
+            #row = [file_name] + row     # maybe modify the csv file first?
+            cursor.execute("INSERT INTO test (%s) VALUES (%s)" %(columns, format_strings), row)
     else:
         continue
 
