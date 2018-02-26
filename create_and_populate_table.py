c5b21ae5483f3007434b4c3c90404e97e4989786
Marly Cormar <marlycormar@gmail.com>
2018-02-26 09:24:57 -0500

    The method create_table now creates a table based on all the field name appearing as the first row of every csv file in a given directory

--- create_and_populate_table.py
+++ create_and_populate_table.py
@@ -2,6 +2,12 @@ import csv
 import MySQLdb
 import os
 
+# connecting to the database
+mydb = MySQLdb.connect(host='localhost', user='root', db='iCARE')
+cursor = mydb.cursor()
+
+# setting the directory to the read the files from
+directory = '/Users/marlycormar/Google Drive/CTS-IT/Tasks/20180220/data'
 
 def create_table(table_name):
     column_names = []
@@ -12,15 +18,17 @@ def create_table(table_name):
             headers = csv_data.next()
 
             if(len(column_names) == 0):
-                column_names = headers
+                column_names = ['`' + field_name  + '`' for field_name in headers]
+
             else:
                 for column in headers:
-                    if not column in column_names:
-                        column_names.append(column)
+                    if not "`" + column + "`" in column_names:
+                        column_names.append("`" + column + "`")
 
-    print(column_names)
+    create_table_query = """CREATE TABLE IF NOT EXISTS """ + table_name + " (" + " VARCHAR(300),".join(column_names) + " VARCHAR(300))"
+    cursor.execute(create_table_query)
 
-def fill_table():
+def fill_table(table_name):
     for file_name in os.listdir(directory):
         if file_name.endswith(".csv") and file_name.startswith("UFH"):
             print(file_name)
@@ -41,21 +49,17 @@ def fill_table():
                 #number_columns += 2 # accounting for columns file_name and study_id
                 #row = [study_id] + row      # consider improving this method of prepending
                 #row = [file_name] + row     # maybe modify the csv file first?
-                cursor.execute("INSERT INTO test (%s) VALUES (%s)" %(columns, format_strings), row)
+                cursor.execute("INSERT INTO %s (%s) VALUES (%s)" %(table_name, columns, format_strings), row)
 
 
-# connecting to the database
-mydb = MySQLdb.connect(host='localhost', user='root', db='iCARE')
-cursor = mydb.cursor()
+table_name = 'farsight'
+create_table(table_name)
+#fill_table(table_name)
 
-# setting the directory to the read the files from
-directory = '/Users/marlycormar/Google Drive/CTS-IT/Tasks/20180220/data'
-
-create_table('some_name')
-#fill_table()
+# commit the the table changes to the db
+mydb.commit()
 
 # close the connection to the database.
-mydb.commit()
 cursor.close()
 
 print("Done");
