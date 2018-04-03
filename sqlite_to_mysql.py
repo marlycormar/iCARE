1de4fd35b56deb7706a9522c3e8bae0f9f43d34e
Marly Cormar <marlycormar@gmail.com>
2018-04-03 15:28:37 -0400

    Improve formatting and add comments

--- sqlite_to_mysql.py
+++ sqlite_to_mysql.py
@@ -3,19 +3,25 @@ import os, re, fileinput, tempfile
 
 
 def dump_sqlite_data(path_to_sqlitedb, path_to_mysql_dump):
+
+    print("Starting: dump_sqlite_data")
+    
     os.system("sqlite3 %s .dump | python cleaning_mysql_dump.py -d malignant > %s" %(path_to_sqlitedb, path_to_mysql_dump))
 
+    print("Done: Sqlite data dumped")
+    print("===================")
+
 def remove_foreing_keys(path_to_mysql_dump):
+    
+    print("Starting: remove_foreing_keys")
+
     # open the source file and read it
     fh = file(path_to_mysql_dump, 'r')
     subject = fh.read()
     fh.close()
 
-    # create the pattern object. Note the "r". In case you're unfamiliar with Python
-    # this is to set the string as raw so we don't have to escape our escape characters
-    pattern = re.compile(r',\nFOREIGN KEY.*$',re.MULTILINE)
-    #print(pattern.findall(subject,re.MULTILINE))
     # do the replace
+    pattern = re.compile(r',\nFOREIGN KEY.*$',re.MULTILINE)
     result = pattern.sub("", subject)
 
     # write the file
@@ -23,15 +29,19 @@ def remove_foreing_keys(path_to_mysql_dump):
     f_out = file(path_to_changed_mysql_dump, 'w')
     f_out.write(result)
     f_out.close()
+    
+    print("Done: Foreing keys removed")
+    print("===================")
 
 def add_key_length(path_to_mysql_dump):
+    
+    print("Starting: add_key_length")
+
     # open the source file and read it
     fh = file(path_to_mysql_dump, 'rw')
     subject = fh.read()
     fh.close()
 
-    # create the pattern object. Note the "r". In case you're unfamiliar with Python
-    # this is to set the string as raw so we don't have to escape our escape characters
     pattern = re.compile(r'PRIMARY KEY \((\`\w\`),(\`\w\`)\)')# "\d*,\d*"
     primary_keys = pattern.findall(subject)
     print(primary_keys)
@@ -49,20 +59,21 @@ def add_key_length(path_to_mysql_dump):
     f_out.write(subject)
     f_out.close()
 
+    print("Done: Key constraint added")
+    print("===================")
+
 def edit_mysql_file(path_to_mysql_dump):
     path = path_to_mysql_dump
     sql_file = open(path,'rw')
     sql = sql_file.read()
 
-
     new_days.write(days)
-
-
     sql_file.close()
 
 path_to_sqlitedb = ''
 path_to_mysql_dump = ''
 
-#dump_sqlite_data(path_to_sqlitedb, path_to_mysql_dump)
-#remove_foreing_keys(path_to_mysql_dump)
-#add_key_length(path_to_mysql_dump + '.copy')
+
+dump_sqlite_data(path_to_sqlitedb, path_to_mysql_dump)
+remove_foreing_keys(path_to_mysql_dump)
+add_key_length(path_to_mysql_dump + '.copy')
