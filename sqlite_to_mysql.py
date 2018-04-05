cceb40075d8932e93f643f80de8e99aa753b61e6
Marly Cormar <marlycormar@gmail.com>
2018-04-05 14:27:32 -0400

    Stop the script and inform the user if an exception is risen

--- sqlite_to_mysql.py
+++ sqlite_to_mysql.py
@@ -10,28 +10,36 @@ mysql_db = os.environ['mysql_db']
 
 def dump_sqlite_data():
     print("Starting: dump_sqlite_data")
-    
-    os.system("sqlite3 %s .dump | python cleaning_mysql_dump.py -d malignant > %s" %(path_to_sqlitedb, path_to_mysql_dump))
+
+    try:
+        os.system("sqlite3 %s .dump | python cleaning_mysql_dump.py -d malignant > %s" %(path_to_sqlitedb, path_to_mysql_dump))
+    except Exception, e:
+        print "The function dump_sqlite_data failed. See error:\n"
+        raise
 
     print("Done: Sqlite data dumped")
     print("===================")
 
 def remove_foreing_keys():
     print("Starting: remove_foreing_keys")
+    
+    try:
+        # open the source file and read it
+        fh = file(path_to_mysql_dump, 'r')
+        subject = fh.read()
+        fh.close()
 
-    # open the source file and read it
-    fh = file(path_to_mysql_dump, 'r')
-    subject = fh.read()
-    fh.close()
+        # do the replace
+        pattern = re.compile(r',\nFOREIGN KEY.*$',re.MULTILINE)
+        result = pattern.sub("", subject)
 
-    # do the replace
-    pattern = re.compile(r',\nFOREIGN KEY.*$',re.MULTILINE)
-    result = pattern.sub("", subject)
-
-    # write the file
-    f_out = file(path_to_mysql_dump, 'w')
-    f_out.write(result)
-    f_out.close()
+        # write the file
+        f_out = file(path_to_mysql_dump, 'w')
+        f_out.write(result)
+        f_out.close()
+    except Exception, e:
+        print "The function remove_foreing_keys failed. See error:\n"
+        raise
     
     print("Done: Foreing keys removed")
     print("===================")
@@ -39,33 +47,37 @@ def remove_foreing_keys():
 def add_key_length():
     print("Starting: add_key_length")
 
-    # open the source file and read it
-    fh = file(path_to_mysql_dump, 'rw')
-    subject = fh.read()
-    fh.close()
-   
-    # todo: improve regex since it will avoid the clean up below
-    # finds PRIMARY KEY (`some_row`, `another row`) and adds
-    #  changes the type TEXT by VARCHAR(200)
-    pattern = re.compile(r'PRIMARY KEY \(\`(.+)\`\)')
-    primary_keys = pattern.findall(subject)
-    primary_keys = [list.replace('`','').split(',') for list in primary_keys]
-    primary_keys = [key for key in itertools.chain.from_iterable(primary_keys)]
-    
-    # todo: here we are changing all the rows with these names
-    # ideally we should change just the rows of the corresponding table
-    for key in primary_keys:
-        pattern = re.compile(r'`%s` (TEXT)' %key)
-        subject = pattern.sub("`%s` VARCHAR(200)"%key, subject)
-   
-    pattern = re.compile(r'(TEXT) PRIMARY KEY')
-    primary_keys = pattern.findall(subject)
-    subject = pattern.sub("VARCHAR(200)", subject)
+    try:
+        # open the source file and read it
+        fh = file(path_to_mysql_dump, 'rw')
+        subject = fh.read()
+        fh.close()
+       
+        # todo: improve regex since it will avoid the clean up below
+        # finds PRIMARY KEY (`some_row`, `another row`) and adds
+        #  changes the type TEXT by VARCHAR(200)
+        pattern = re.compile(r'PRIMARY KEY \(\`(.+)\`\)')
+        primary_keys = pattern.findall(subject)
+        primary_keys = [list.replace('`','').split(',') for list in primary_keys]
+        primary_keys = [key for key in itertools.chain.from_iterable(primary_keys)]
+        
+        # todo: here we are changing all the rows with these names
+        # ideally we should change just the rows of the corresponding table
+        for key in primary_keys:
+            pattern = re.compile(r'`%s` (TEXT)' %key)
+            subject = pattern.sub("`%s` VARCHAR(200)"%key, subject)
+       
+        pattern = re.compile(r'(TEXT) PRIMARY KEY')
+        primary_keys = pattern.findall(subject)
+        subject = pattern.sub("VARCHAR(200)", subject)
 
-    # write the file
-    f_out = file(path_to_mysql_dump, 'w')
-    f_out.write(subject)
-    f_out.close()
+        # write the file
+        f_out = file(path_to_mysql_dump, 'w')
+        f_out.write(subject)
+        f_out.close()
+    except Exception, e:
+        print "The function add_key_length failed. See error:\n"
+        raise
 
     print("Done: Key constraint added")
     print("===================")
@@ -73,7 +85,11 @@ def add_key_length():
 def copy_data_to_mysql():
     print("Starting: copy_data_to_mysql")
     
-    os.system("mysql -u %s -p%s %s < %s" %(mysql_user, mysql_password, mysql_db, path_to_mysql_dump))
+    try:
+        os.system("mysql -u %s -p%s %s < %s" %(mysql_user, mysql_password, mysql_db, path_to_mysql_dump))
+    except Exception, e:
+        print "The function copy_data_to_mysql failed. See error:\n"
+        raise
 
     print("Done: Sql data copied to mysql")
     print("===================")
