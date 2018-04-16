986d94a0986caced790f6a08e66778ce834401bb
Marly Cormar <marlycormar@gmail.com>
2018-04-16 12:05:08 -0400

    Include env variables

--- script_table_creation.py
+++ script_table_creation.py
@@ -2,3 +2,8 @@ import csv
 import MySQLdb
 import os
 
+# read configuration from environment
+mysql_db = os.environ['mysql_db']
+directory = os.environ['directory_with_farsight_files']
+sql_dump = os.environ['sql_dump']
+table_name = 'farsight'
